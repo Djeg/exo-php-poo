b7cf1046c8d2247b193ec37c011def09a7b5e01a
David Jegat <david.jegat@gmail.com>
2021-10-12 17:05:19 +0200

    Ajout de la correction de l'exo HTML

--- public/index.php
+++ public/index.php
@@ -1,13 +1,19 @@
 <?php
 
+
 require './vendor/autoload.php';
 
-$test = new App\Test();
+use App\HTML\Element;
+use App\HTML\Form;
 
-use App\Game\Personnage;
+$separator = new Element('div', ['class' => 'form-control']);
+$form = new Form($separator);
 
-$arthur = new Personnage('Arthur');
+echo $form->start();
+echo $form->widget('Email :', 'email');
+echo $form->widget('Mot de passe :', 'password');
 
-$arthur->afficher();
-
-var_dump($test);
+echo $form->separatorStart();
+echo $form->submitButton('Envoyer');
+echo $form->separatorEnd();
+echo $form->end();
---
+++ src/Toto.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Game\Personnage;
+
+class Toto
+{
+    public function test(Personnage $personnage): void
+    {
+
+    }
+}
