246fa34a23639557282a12c28707b571155bf74c
David Jegat <david.jegat@gmail.com>
2022-01-10 09:17:56 +0100

    Initial commit

--- public/index.php
+++ public/index.php
@@ -2,18 +2,3 @@
 
 
 require './vendor/autoload.php';
-
-use App\HTML\Element;
-use App\HTML\Form;
-
-$separator = new Element('div', ['class' => 'form-control']);
-$form = new Form($separator);
-
-echo $form->start();
-echo $form->widget('Email :', 'email');
-echo $form->widget('Mot de passe :', 'password');
-
-echo $form->separatorStart();
-echo $form->submitButton('Envoyer');
-echo $form->separatorEnd();
-echo $form->end();
